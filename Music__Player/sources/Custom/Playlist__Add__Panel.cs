66e1fb41e4c049e70e9faeeecb91ac635dfed2ca
AmosIvor <trantuanvu00000@gmail.com>
2022-10-25 20:29:12 +0700

    add Playlist__Add__Panel as UserControl

---
+++ Music__Player/sources/Custom/Playlist__Add__Panel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music__Player.sources.Custom
+{
+    public partial class Playlist__Add__Panel : UserControl
+    {
+        public Playlist__Add__Panel()
+        {
+            InitializeComponent();
+        }
+    }
+}
