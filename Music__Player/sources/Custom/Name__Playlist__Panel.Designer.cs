9f6a98e37e5d7051e72f10a1c28a4db49d6c1341
AmosIvor <trantuanvu00000@gmail.com>
2022-12-17 11:13:25 +0700

    add: child component in Dropdown Playlist

---
+++ Music__Player/sources/Custom/Name__Playlist__Panel.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Music__Player.sources.Custom
+{
+    partial class Name__Playlist__Panel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnImage = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.lblName);
+            this.pnlMain.Controls.Add(this.btnImage);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.ShadowDecoration.Parent = this.pnlMain;
+            this.pnlMain.Size = new System.Drawing.Size(287, 46);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoEllipsis = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(46, 6);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(223, 30);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Tran Tuan Vu";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnImage
+            // 
+            this.btnImage.CheckedState.Parent = this.btnImage;
+            this.btnImage.HoverState.Parent = this.btnImage;
+            this.btnImage.Image = global::Music__Player.Properties.Resources.icon_playlist;
+            this.btnImage.ImageSize = new System.Drawing.Size(24, 24);
+            this.btnImage.Location = new System.Drawing.Point(3, 6);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.PressedState.Parent = this.btnImage;
+            this.btnImage.Size = new System.Drawing.Size(35, 35);
+            this.btnImage.TabIndex = 0;
+            // 
+            // Name__Playlist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.pnlMain);
+            this.Margin = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.Name = "Name__Playlist";
+            this.Size = new System.Drawing.Size(287, 46);
+            this.pnlMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel pnlMain;
+        private Guna.UI2.WinForms.Guna2ImageButton btnImage;
+        private System.Windows.Forms.Label lblName;
+    }
+}
---
+++ Music__Player/sources/Custom/Name__Playlist__Panel.cs
@@ -0,0 +1,165 @@
+﻿using Music__Player.sources.Constant;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music__Player.sources.Custom
+{
+    public partial class Name__Playlist__Panel : UserControl
+    {
+        Color background = Color.FromArgb(192, 192, 255);
+        public Name__Playlist__Panel()
+        {
+            InitializeComponent();
+        }
+
+        public Name__Playlist__Panel(DataRow row)
+        {
+            InitializeComponent();
+
+            this.Name_Playlist = row["NAME_PLAYLIST"].ToString();
+
+            this.IsCreated = false;
+
+            LoadInitialEvent();
+        }
+
+        public Name__Playlist__Panel(string name_Playlist)
+        {
+            InitializeComponent();
+
+            this.Name_Playlist = name_Playlist;
+
+            this.IsCreated = false;
+
+            LoadInitialEvent();
+        }
+
+        void LoadInitialEvent()
+        {
+            pnlMain.MouseEnter += userControl_MouseEnter;
+
+            pnlMain.MouseLeave += userControl_MouseLeave;
+        }
+
+        private string name_Playlist;
+        public string Name_Playlist
+        {
+            get { return name_Playlist;}
+
+            set { name_Playlist = value; lblName.Text = name_Playlist; }
+        }
+
+        private bool isCreated;
+        public bool IsCreated
+        {
+            get { return isCreated;}
+
+            set
+            {
+                isCreated = value;
+
+                if (isCreated == true)
+                {
+                    btnImage.Image = ICON.Playlist_Create;
+                }
+                else
+                {
+                    btnImage.Image = ICON.Playlist;
+                }
+            }
+        }
+
+        private bool isHovered;
+        public bool IsHovered
+        {
+            get { return isHovered;}
+
+            set
+            {
+                isHovered = value;
+
+                if (isHovered == false)
+                {
+                    pnlMain.FillColor = background;
+                }
+
+                else
+                {
+                    pnlMain.FillColor = Color.FromArgb(20, 0, 0, 0);
+                }
+            }
+        }
+
+        private event EventHandler _mouseEnterAdd;
+
+        public event EventHandler MouseEnterAdd
+        {
+            add
+            {
+                this.Cursor = Cursors.Hand;
+
+                _mouseEnterAdd += value;
+
+                foreach (Control control in pnlMain.Controls)
+                {
+                    control.MouseEnter += value;
+
+                    control.Cursor = Cursors.Hand;
+                }
+            }
+
+            remove
+            {
+                this.Cursor = Cursors.Default;
+
+                _mouseEnterAdd -= value;
+
+                foreach (Control control in pnlMain.Controls)
+                {
+                    control.MouseEnter -= value;
+
+                    Cursor = Cursors.Default;
+                }
+            }
+        }
+
+
+        private event EventHandler _mouseLeaveAdd;
+
+        public event EventHandler MouseLeaveAdd
+        {
+            add
+            {
+                _mouseLeaveAdd += value;
+            }
+
+            remove
+            {
+                _mouseLeaveAdd -= value;
+            }
+        }
+
+        #region Event
+
+        private void userControl_MouseEnter(object sender, EventArgs e)
+        {
+            _mouseEnterAdd?.Invoke(sender, e);
+        }
+
+        private void userControl_MouseLeave(object sender, EventArgs e)
+        {
+            _mouseLeaveAdd?.Invoke(sender, e);
+        }
+
+        #endregion
+
+    }
+}
