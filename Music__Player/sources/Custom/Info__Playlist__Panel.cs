6ca5688d6942dcfab0c1f13a6fdfe857d55e244e
AmosIvor <trantuanvu00000@gmail.com>
2022-12-19 22:59:19 +0700

    add: some properties: Name, Image, Quantity Song and connect SQL server

--- Music__Player/sources/Custom/Info__Playlist__Panel.cs
+++ Music__Player/sources/Custom/Info__Playlist__Panel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Music__Player.sources.Constant;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +18,51 @@ namespace Music__Player.sources.Custom
         {
             InitializeComponent();
         }
+
+        public Info__Playlist__Panel(DataRow row)
+        {
+            InitializeComponent();
+
+            this.Name_Playlist = row["NAME_PLAYLIST"].ToString();
+
+            this.Number_Song = row["QUANTITY"].ToString() + " songs";
+
+            if (!Convert.IsDBNull(row["IMAGE_PLAYLIST"]))
+            {
+                MemoryStream stream = new MemoryStream((Byte[])row["IMAGE_PLAYLIST"]);
+
+                Image temp = Image.FromStream(stream);
+
+                this.Image_Playlist = temp;
+            }
+            else
+            {
+                this.Image_Playlist = IMAGE.DefaultPlaylist;
+            }
+        }
+        private string name_Playlist;
+        public string Name_Playlist
+        {
+            get { return name_Playlist; }
+
+            set { name_Playlist = value; lblName.Text = value; }
+        }
+
+        private string number_Song;
+        public string Number_Song
+        {
+            get { return number_Song; }
+            
+            set { number_Song = value; lblNumber.Text = value; }
+        }
+
+        private Image image_Playlist;
+        public Image Image_Playlist
+        {
+            get { return image_Playlist;}
+
+            set { image_Playlist = value; pbImage.Image = image_Playlist; }
+        }
+        
     }
 }
