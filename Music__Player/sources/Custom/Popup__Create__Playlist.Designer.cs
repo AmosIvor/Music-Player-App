5dd406b160d8cf7b0824dfcf8047843057ada9e7
AmosIvor <trantuanvu00000@gmail.com>
2022-12-19 23:02:52 +0700

    add: event Load, click Create button and check event text change in textbox when user enter name

--- Music__Player/sources/Custom/Popup__Create__Playlist.Designer.cs
+++ Music__Player/sources/Custom/Popup__Create__Playlist.Designer.cs
@@ -32,11 +32,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Popup__Create__Playlist));
             this.guna2GradientPanel2 = new Guna.UI2.WinForms.Guna2GradientPanel();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txbNamePlaylist = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2Panel5 = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.btnCreate = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Panel6 = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2CircleButton2 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.btnExit = new Guna.UI2.WinForms.Guna2CircleButton();
             this.label2 = new System.Windows.Forms.Label();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2GradientPanel2.SuspendLayout();
@@ -63,46 +63,47 @@
             // 
             // guna2Panel1
             // 
-            this.guna2Panel1.Controls.Add(this.guna2TextBox2);
+            this.guna2Panel1.Controls.Add(this.txbNamePlaylist);
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.guna2Panel1.Location = new System.Drawing.Point(0, 75);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
             this.guna2Panel1.Size = new System.Drawing.Size(397, 128);
-            this.guna2Panel1.TabIndex = 3;
+            this.guna2Panel1.TabIndex = 0;
             // 
-            // guna2TextBox2
+            // txbNamePlaylist
             // 
-            this.guna2TextBox2.AutoSize = true;
-            this.guna2TextBox2.BorderRadius = 20;
-            this.guna2TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.guna2TextBox2.DefaultText = "";
-            this.guna2TextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.guna2TextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.guna2TextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox2.DisabledState.Parent = this.guna2TextBox2;
-            this.guna2TextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.Black;
-            this.guna2TextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox2.FocusedState.Parent = this.guna2TextBox2;
-            this.guna2TextBox2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2TextBox2.ForeColor = System.Drawing.Color.Black;
-            this.guna2TextBox2.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox2.HoverState.Parent = this.guna2TextBox2;
-            this.guna2TextBox2.Location = new System.Drawing.Point(19, 35);
-            this.guna2TextBox2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
-            this.guna2TextBox2.Name = "guna2TextBox2";
-            this.guna2TextBox2.PasswordChar = '\0';
-            this.guna2TextBox2.PlaceholderForeColor = System.Drawing.Color.Black;
-            this.guna2TextBox2.PlaceholderText = "Enter name playlist";
-            this.guna2TextBox2.SelectedText = "";
-            this.guna2TextBox2.ShadowDecoration.Parent = this.guna2TextBox2;
-            this.guna2TextBox2.Size = new System.Drawing.Size(358, 59);
-            this.guna2TextBox2.TabIndex = 0;
-            this.guna2TextBox2.TextOffset = new System.Drawing.Point(20, 0);
+            this.txbNamePlaylist.AutoSize = true;
+            this.txbNamePlaylist.BorderRadius = 20;
+            this.txbNamePlaylist.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbNamePlaylist.DefaultText = "";
+            this.txbNamePlaylist.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txbNamePlaylist.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txbNamePlaylist.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbNamePlaylist.DisabledState.Parent = this.txbNamePlaylist;
+            this.txbNamePlaylist.DisabledState.PlaceholderForeColor = System.Drawing.Color.Black;
+            this.txbNamePlaylist.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbNamePlaylist.FocusedState.Parent = this.txbNamePlaylist;
+            this.txbNamePlaylist.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNamePlaylist.ForeColor = System.Drawing.Color.Black;
+            this.txbNamePlaylist.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbNamePlaylist.HoverState.Parent = this.txbNamePlaylist;
+            this.txbNamePlaylist.Location = new System.Drawing.Point(19, 35);
+            this.txbNamePlaylist.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txbNamePlaylist.Name = "txbNamePlaylist";
+            this.txbNamePlaylist.PasswordChar = '\0';
+            this.txbNamePlaylist.PlaceholderForeColor = System.Drawing.Color.Black;
+            this.txbNamePlaylist.PlaceholderText = "Enter name playlist";
+            this.txbNamePlaylist.SelectedText = "";
+            this.txbNamePlaylist.ShadowDecoration.Parent = this.txbNamePlaylist;
+            this.txbNamePlaylist.Size = new System.Drawing.Size(358, 59);
+            this.txbNamePlaylist.TabIndex = 0;
+            this.txbNamePlaylist.TextOffset = new System.Drawing.Point(20, 0);
+            this.txbNamePlaylist.TextChanged += new System.EventHandler(this.txbNamePlaylist_TextChanged);
             // 
             // guna2Panel5
             // 
-            this.guna2Panel5.Controls.Add(this.guna2Button2);
+            this.guna2Panel5.Controls.Add(this.btnCreate);
             this.guna2Panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.guna2Panel5.Location = new System.Drawing.Point(0, 203);
             this.guna2Panel5.Name = "guna2Panel5";
@@ -110,25 +111,26 @@
             this.guna2Panel5.Size = new System.Drawing.Size(397, 100);
             this.guna2Panel5.TabIndex = 2;
             // 
-            // guna2Button2
+            // btnCreate
             // 
-            this.guna2Button2.BorderRadius = 20;
-            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
-            this.guna2Button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
-            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2Button2.ForeColor = System.Drawing.Color.White;
-            this.guna2Button2.HoverState.Parent = this.guna2Button2;
-            this.guna2Button2.Location = new System.Drawing.Point(19, 15);
-            this.guna2Button2.Name = "guna2Button2";
-            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
-            this.guna2Button2.Size = new System.Drawing.Size(358, 59);
-            this.guna2Button2.TabIndex = 0;
-            this.guna2Button2.Text = "CREATE";
+            this.btnCreate.BorderRadius = 20;
+            this.btnCreate.CheckedState.Parent = this.btnCreate;
+            this.btnCreate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCreate.CustomImages.Parent = this.btnCreate;
+            this.btnCreate.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.ForeColor = System.Drawing.Color.White;
+            this.btnCreate.HoverState.Parent = this.btnCreate;
+            this.btnCreate.Location = new System.Drawing.Point(19, 15);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.ShadowDecoration.Parent = this.btnCreate;
+            this.btnCreate.Size = new System.Drawing.Size(358, 59);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "CREATE";
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // guna2Panel6
             // 
-            this.guna2Panel6.Controls.Add(this.guna2CircleButton2);
+            this.guna2Panel6.Controls.Add(this.btnExit);
             this.guna2Panel6.Controls.Add(this.label2);
             this.guna2Panel6.Dock = System.Windows.Forms.DockStyle.Top;
             this.guna2Panel6.Location = new System.Drawing.Point(0, 0);
@@ -137,23 +139,24 @@
             this.guna2Panel6.Size = new System.Drawing.Size(397, 75);
             this.guna2Panel6.TabIndex = 1;
             // 
-            // guna2CircleButton2
+            // btnExit
             // 
-            this.guna2CircleButton2.CheckedState.Parent = this.guna2CircleButton2;
-            this.guna2CircleButton2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.guna2CircleButton2.CustomImages.Parent = this.guna2CircleButton2;
-            this.guna2CircleButton2.FillColor = System.Drawing.Color.Transparent;
-            this.guna2CircleButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.guna2CircleButton2.ForeColor = System.Drawing.Color.White;
-            this.guna2CircleButton2.HoverState.Parent = this.guna2CircleButton2;
-            this.guna2CircleButton2.Image = ((System.Drawing.Image)(resources.GetObject("guna2CircleButton2.Image")));
-            this.guna2CircleButton2.ImageSize = new System.Drawing.Size(16, 16);
-            this.guna2CircleButton2.Location = new System.Drawing.Point(370, 6);
-            this.guna2CircleButton2.Name = "guna2CircleButton2";
-            this.guna2CircleButton2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.guna2CircleButton2.ShadowDecoration.Parent = this.guna2CircleButton2;
-            this.guna2CircleButton2.Size = new System.Drawing.Size(24, 24);
-            this.guna2CircleButton2.TabIndex = 1;
+            this.btnExit.CheckedState.Parent = this.btnExit;
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.CustomImages.Parent = this.btnExit;
+            this.btnExit.FillColor = System.Drawing.Color.Transparent;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.HoverState.Parent = this.btnExit;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageSize = new System.Drawing.Size(16, 16);
+            this.btnExit.Location = new System.Drawing.Point(370, 6);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnExit.ShadowDecoration.Parent = this.btnExit;
+            this.btnExit.Size = new System.Drawing.Size(24, 24);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // label2
             // 
@@ -170,7 +173,7 @@
             this.guna2Elipse1.BorderRadius = 15;
             this.guna2Elipse1.TargetControl = this;
             // 
-            // Popup__CreatePlaylist
+            // Popup__Create__Playlist
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -178,7 +181,7 @@
             this.ClientSize = new System.Drawing.Size(402, 307);
             this.Controls.Add(this.guna2GradientPanel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Popup__CreatePlaylist";
+            this.Name = "Popup__Create__Playlist";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Popup__CreatePlaylist";
             this.TopMost = true;
@@ -196,11 +199,11 @@
 
         private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel2;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
-        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+        private Guna.UI2.WinForms.Guna2TextBox txbNamePlaylist;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel5;
-        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button btnCreate;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel6;
-        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton2;
+        private Guna.UI2.WinForms.Guna2CircleButton btnExit;
         private System.Windows.Forms.Label label2;
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
     }
--- Music__Player/sources/Custom/Popup__Create__Playlist.cs
+++ Music__Player/sources/Custom/Popup__Create__Playlist.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Music__Player.sources.DAO.CustomDAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,54 @@ namespace Music__Player.sources.Custom
 {
     public partial class Popup__Create__Playlist : Form
     {
+        
         public Popup__Create__Playlist()
         {
             InitializeComponent();
+
+            LoadPopup();
+        }
+
+        void LoadPopup()
+        {
+            txbNamePlaylist.Text = "";
+
+            txbNamePlaylist.Focus();
+
+            btnCreate.Enabled = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txbNamePlaylist_TextChanged(object sender, EventArgs e)
+        {
+            if (txbNamePlaylist.Text == "")
+            {
+                btnCreate.Enabled = false;
+
+                return;
+            }
+
+            btnCreate.Enabled = true;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            int data = Popup__Create__Playlist__DAO.Instance.InsertPlaylist(txbNamePlaylist.Text);
+
+            if (data >= 0)
+            {
+                MessageBox.Show("Name playlist already exist");
+
+                txbNamePlaylist.Focus();
+
+                return;
+            }
+
+            this.Close();
         }
     }
 }
