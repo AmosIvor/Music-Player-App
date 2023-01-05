3664f0e184e9493f705e42e67848542a0063ca56
AmosIvor <trantuanvu00000@gmail.com>
2023-01-05 22:25:49 +0700

    add: Song__History__DAO and initial static variable instance class

---
+++ Music__Player/sources/DAO/CustomDAO/Song__History__DAO.cs
@@ -0,0 +1,32 @@
+﻿using Music__Player.sources.Custom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music__Player.sources.DAO.CustomDAO
+{
+    public class Song__History__DAO
+    {
+        private static Song__History__DAO instance;
+        public static Song__History__DAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Song__History__DAO();
+                return instance;
+            }
+            private set { instance = value; }
+        }
+        private Song__History__DAO() { }
+        public List<Song__History> GetListSongHistory()
+        {
+            List<Song__History> listSongHistory = new List<Song__History>();
+
+
+            return listSongHistory;
+        }
+    }
+}
