8e7b97ba981e493123ad92fd7aae6885f776921a
AmosIvor <trantuanvu00000@gmail.com>
2023-01-18 22:05:01 +0700

    add: icon heart blue and green

--- Music__Player/Properties/Resources.Designer.cs
+++ Music__Player/Properties/Resources.Designer.cs
@@ -403,6 +403,26 @@ namespace Music__Player.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon_heart_blue {
+            get {
+                object obj = ResourceManager.GetObject("icon_heart_blue", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_heart_green {
+            get {
+                object obj = ResourceManager.GetObject("icon_heart_green", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icon_history_blue {
             get {
                 object obj = ResourceManager.GetObject("icon_history_blue", resourceCulture);
