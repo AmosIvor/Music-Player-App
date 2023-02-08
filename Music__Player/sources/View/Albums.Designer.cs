92b509f05b1077cb754d630c52f10fe35302e46f
AmosIvor <trantuanvu00000@gmail.com>
2023-02-08 17:07:28 +0700

    update: re-design UI Album and implement event

--- Music__Player/sources/View/Albums.Designer.cs
+++ Music__Player/sources/View/Albums.Designer.cs
@@ -28,72 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.album__Panel1 = new Music__Player.sources.Custom.Album__Panel();
-            this.album__Panel2 = new Music__Player.sources.Custom.Album__Panel();
-            this.album__Panel3 = new Music__Player.sources.Custom.Album__Panel();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnBack = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.btnNext = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.pnlBottom = new Guna.UI2.WinForms.Guna2Panel();
+            this.songPlayingBottomBar = new Music__Player.sources.Custom.Song__Playing__BottomBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fpnlArtist = new System.Windows.Forms.FlowLayoutPanel();
+            this.fpnlGenre = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.guna2Panel2.SuspendLayout();
+            this.pnlBottom.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // flowLayoutPanel1
+            // guna2Panel2
             // 
-            this.flowLayoutPanel1.Controls.Add(this.album__Panel1);
-            this.flowLayoutPanel1.Controls.Add(this.album__Panel2);
-            this.flowLayoutPanel1.Controls.Add(this.album__Panel3);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1359, 965);
-            this.flowLayoutPanel1.TabIndex = 0;
+            this.guna2Panel2.BackColor = System.Drawing.Color.Lavender;
+            this.guna2Panel2.Controls.Add(this.btnBack);
+            this.guna2Panel2.Controls.Add(this.btnNext);
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel2.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.ShadowDecoration.Parent = this.guna2Panel2;
+            this.guna2Panel2.Size = new System.Drawing.Size(1359, 76);
+            this.guna2Panel2.TabIndex = 2;
             // 
-            // album__Panel1
+            // btnBack
             // 
-            this.album__Panel1.BackColor = System.Drawing.Color.Gainsboro;
-            this.album__Panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.album__Panel1.Location = new System.Drawing.Point(3, 3);
-            this.album__Panel1.Name = "album__Panel1";
-            this.album__Panel1.Size = new System.Drawing.Size(1359, 363);
-            this.album__Panel1.TabIndex = 0;
-            this.album__Panel1.TypeAlbum = null;
+            this.btnBack.CheckedState.ImageSize = new System.Drawing.Size(34, 34);
+            this.btnBack.CheckedState.Parent = this.btnBack;
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.HoverState.ImageSize = new System.Drawing.Size(36, 36);
+            this.btnBack.HoverState.Parent = this.btnBack;
+            this.btnBack.Image = global::Music__Player.Properties.Resources.icon_back_blue;
+            this.btnBack.ImageSize = new System.Drawing.Size(34, 34);
+            this.btnBack.Location = new System.Drawing.Point(40, 18);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.PressedState.ImageSize = new System.Drawing.Size(34, 34);
+            this.btnBack.PressedState.Parent = this.btnBack;
+            this.btnBack.Size = new System.Drawing.Size(36, 36);
+            this.btnBack.TabIndex = 0;
             // 
-            // album__Panel2
+            // btnNext
             // 
-            this.album__Panel2.BackColor = System.Drawing.Color.Gainsboro;
-            this.album__Panel2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.album__Panel2.Location = new System.Drawing.Point(3, 372);
-            this.album__Panel2.Name = "album__Panel2";
-            this.album__Panel2.Size = new System.Drawing.Size(1359, 363);
-            this.album__Panel2.TabIndex = 1;
-            this.album__Panel2.TypeAlbum = null;
+            this.btnNext.CheckedState.ImageSize = new System.Drawing.Size(34, 34);
+            this.btnNext.CheckedState.Parent = this.btnNext;
+            this.btnNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNext.HoverState.ImageSize = new System.Drawing.Size(36, 36);
+            this.btnNext.HoverState.Parent = this.btnNext;
+            this.btnNext.Image = global::Music__Player.Properties.Resources.icon_skip_blue;
+            this.btnNext.ImageSize = new System.Drawing.Size(34, 34);
+            this.btnNext.Location = new System.Drawing.Point(91, 18);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.PressedState.ImageSize = new System.Drawing.Size(34, 34);
+            this.btnNext.PressedState.Parent = this.btnNext;
+            this.btnNext.Size = new System.Drawing.Size(36, 36);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
-            // album__Panel3
+            // pnlBottom
             // 
-            this.album__Panel3.BackColor = System.Drawing.Color.Gainsboro;
-            this.album__Panel3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.album__Panel3.Location = new System.Drawing.Point(3, 741);
-            this.album__Panel3.Name = "album__Panel3";
-            this.album__Panel3.Size = new System.Drawing.Size(1359, 363);
-            this.album__Panel3.TabIndex = 2;
-            this.album__Panel3.TypeAlbum = null;
+            this.pnlBottom.BackColor = System.Drawing.Color.Transparent;
+            this.pnlBottom.Controls.Add(this.songPlayingBottomBar);
+            this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlBottom.Location = new System.Drawing.Point(0, 865);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.ShadowDecoration.Parent = this.pnlBottom;
+            this.pnlBottom.Size = new System.Drawing.Size(1359, 100);
+            this.pnlBottom.TabIndex = 3;
+            // 
+            // songPlayingBottomBar
+            // 
+            this.songPlayingBottomBar.BackColor = System.Drawing.Color.Transparent;
+            this.songPlayingBottomBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.songPlayingBottomBar.IsPlay = false;
+            this.songPlayingBottomBar.Location = new System.Drawing.Point(0, 0);
+            this.songPlayingBottomBar.Name = "songPlayingBottomBar";
+            this.songPlayingBottomBar.Size = new System.Drawing.Size(1359, 100);
+            this.songPlayingBottomBar.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.fpnlArtist);
+            this.panel1.Controls.Add(this.fpnlGenre);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1359, 789);
+            this.panel1.TabIndex = 4;
+            // 
+            // fpnlArtist
+            // 
+            this.fpnlArtist.Location = new System.Drawing.Point(46, 447);
+            this.fpnlArtist.Name = "fpnlArtist";
+            this.fpnlArtist.Size = new System.Drawing.Size(1294, 303);
+            this.fpnlArtist.TabIndex = 5;
+            // 
+            // fpnlGenre
+            // 
+            this.fpnlGenre.Location = new System.Drawing.Point(43, 82);
+            this.fpnlGenre.Name = "fpnlGenre";
+            this.fpnlGenre.Size = new System.Drawing.Size(1294, 303);
+            this.fpnlGenre.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(38, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Genre";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Cooper Black", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(38, 403);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 26);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Artist";
             // 
             // Albums
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlBottom);
+            this.Controls.Add(this.guna2Panel2);
             this.Name = "Albums";
             this.Size = new System.Drawing.Size(1359, 965);
-            this.Load += new System.EventHandler(this.Albums_Load);
-            this.flowLayoutPanel1.ResumeLayout(false);
+            this.guna2Panel2.ResumeLayout(false);
+            this.pnlBottom.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private Custom.Album__Panel album__Panel1;
-        private Custom.Album__Panel album__Panel2;
-        private Custom.Album__Panel album__Panel3;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2ImageButton btnBack;
+        private Guna.UI2.WinForms.Guna2ImageButton btnNext;
+        private Guna.UI2.WinForms.Guna2Panel pnlBottom;
+        private Custom.Song__Playing__BottomBar songPlayingBottomBar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel fpnlArtist;
+        private System.Windows.Forms.FlowLayoutPanel fpnlGenre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
     }
 }
--- Music__Player/sources/View/Albums.cs
+++ Music__Player/sources/View/Albums.cs
@@ -1,5 +1,7 @@
-﻿using Music__Player.sources.Custom;
+﻿using Guna.UI2.WinForms;
+using Music__Player.sources.Custom;
 using Music__Player.sources.DAO.AlbumDAO;
+using Music__Player.sources.DAO.CustomDAO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,27 +16,300 @@ namespace Music__Player.sources.View
 {
     public partial class Albums : UserControl
     {
+        Panel pnlGenreHovered = new Panel();
+
+        Panel pnlClicked = new Panel();
+
+        Info__Album currInfoAlbumPlaying;
+
         public Albums()
         {
             InitializeComponent();
-            Load_Album();
+
+            LoadAlbum();
+
+            LoadEventClick();
+
+            LoadInitialSongBottomBar();
         }
 
-        private void Albums_Load(object sender, EventArgs e)
+        public void LoadAlbum()
         {
-            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
-            flowLayoutPanel1.WrapContents = false;
-            flowLayoutPanel1.AutoScroll = true;
+            LoadAlbumGenre();
+
+            LoadAlbumArtist();
         }
 
-        private void Load_Album()
+        #region Album Genre
+
+        void LoadAlbumGenre()
         {
-            flowLayoutPanel1.Controls.Clear();
-            List<Album__Panel> listAlbums = AlbumDisplayDAO.Instance.GetAlbumsPanel();
-            foreach (Album__Panel album in listAlbums)
+            List<Info__Album> listInfoAlbum = Info__Album__DAO.Instance.GetListInfoAlbumGenre();
+
+            foreach (Info__Album album in listInfoAlbum)
             {
-                flowLayoutPanel1.Controls.Add(album);
+                album.MouseDoubleClickAdd += album_MouseDoubleClickAdd;
+
+                album.MouseEnterAdd += album_MouseEnterAdd;
+
+                album.MouseLeaveAdd += album_MouseLeaveAdd;
+
+                album.MouseClickPlay += album_MouseClickPlay;
+
+                fpnlGenre.Controls.Add(album);
             }
         }
+
+        void LoadAlbumArtist()
+        {
+            List<Info__Album> listInfoAlbum = Info__Album__DAO.Instance.GetListInfoAlbumArtist();
+
+            foreach (Info__Album album in listInfoAlbum)
+            {
+                album.MouseDoubleClickAdd += album_MouseDoubleClickAdd;
+
+                album.MouseEnterAdd += album_MouseEnterAdd;
+
+                album.MouseLeaveAdd += album_MouseLeaveAdd;
+
+                album.MouseClickPlay += album_MouseClickPlay;
+
+                fpnlArtist.Controls.Add(album);
+            }
+        }
+
+        private void album_MouseClickPlay(object sender, MouseEventArgs e)
+        {
+            if (fpnlGenre.Tag != null)
+            {
+                Info__Album prev = (Info__Album)fpnlGenre.Tag;
+
+                prev.IsSelected = false;
+
+                prev.IsHovered = false;
+            }
+
+            Info__Album curr = Info__Album__DAO.Instance.GetInfoAlbumFromControlIntoPanel(sender);
+
+            if (currInfoAlbumPlaying == curr)
+            {
+                Navigate.Navigation.Instance.mainScreen.LoadChildAlbum();
+
+                pnlClicked.Tag = curr;
+
+                return;
+            }
+
+            curr.IsSelected = true;
+
+            fpnlGenre.Tag = curr;
+
+            currInfoAlbumPlaying = curr;
+
+            pnlClicked.Tag = curr;
+
+            Navigate.Navigation.Instance.mainScreen.LoadChildAlbum();
+
+            if (curr.IsArtist == false)
+            {
+                AlbumDAO.Instance.isArtist = false;
+
+                Navigate.Navigation.Instance.childAlbumScreen.LoadTitle(curr.Image_Genre, curr.Title);
+
+                Navigate.Navigation.Instance.childAlbumScreen.LoadChildAlbum(curr.ID_Genre.ToString());
+            }
+            else
+            {
+                AlbumDAO.Instance.isArtist = true;
+
+                Navigate.Navigation.Instance.childAlbumScreen.LoadTitle(curr.Image_Genre, curr.Title);
+
+                Navigate.Navigation.Instance.childAlbumScreen.LoadChildAlbum(curr.Title);
+            }
+
+            Navigate.Navigation.Instance.childAlbumScreen.PlayFirstSong();
+        }
+        private void album_MouseDoubleClickAdd(object sender, MouseEventArgs e)
+        {
+            if (fpnlGenre.Tag != null)
+            {
+                Info__Album prevSelected = (Info__Album)fpnlGenre.Tag;
+
+                prevSelected.IsSelected = false;
+
+                prevSelected.IsHovered = false;
+            }
+
+            if (sender is Label || sender is Guna2ImageButton || sender is Guna2PictureBox)
+            {
+                Info__Album infoAlbumInside = Info__Album__DAO.Instance.GetInfoAlbumFromControlIntoPanel(sender);
+
+                if (currInfoAlbumPlaying == infoAlbumInside)
+                {
+                    Navigate.Navigation.Instance.mainScreen.LoadChildAlbum();
+
+                    pnlClicked.Tag = infoAlbumInside;
+
+                    return;
+                }
+
+                infoAlbumInside.IsSelected = true;
+
+                fpnlGenre.Tag = infoAlbumInside;
+
+                currInfoAlbumPlaying = infoAlbumInside;
+
+                pnlClicked.Tag = infoAlbumInside;
+
+                Navigate.Navigation.Instance.mainScreen.LoadChildAlbum();
+
+                if (infoAlbumInside.IsArtist == false)
+                {
+                    AlbumDAO.Instance.isArtist = false;
+
+                    Navigate.Navigation.Instance.childAlbumScreen.LoadTitle(infoAlbumInside.Image_Genre, infoAlbumInside.Title);
+
+                    Navigate.Navigation.Instance.childAlbumScreen.LoadChildAlbum(infoAlbumInside.ID_Genre.ToString());
+                }
+                else
+                {
+                    AlbumDAO.Instance.isArtist = true;
+
+                    Navigate.Navigation.Instance.childAlbumScreen.LoadTitle(infoAlbumInside.Image_Genre, infoAlbumInside.Title);
+
+                    Navigate.Navigation.Instance.childAlbumScreen.LoadChildAlbum(infoAlbumInside.Title);
+                }
+
+                Navigate.Navigation.Instance.childAlbumScreen.PlayFirstSong();
+
+                return;
+            }
+
+            Info__Album infoAlbumOutside = Info__Album__DAO.Instance.GetInfoAlbumFromPanel(sender);
+
+            if (currInfoAlbumPlaying == infoAlbumOutside)
+            {
+                Navigate.Navigation.Instance.mainScreen.LoadChildAlbum();
+
+                pnlClicked.Tag = infoAlbumOutside;
+
+                return;
+            }
+
+            fpnlGenre.Tag = infoAlbumOutside;
+
+            infoAlbumOutside.IsSelected = true;
+
+            currInfoAlbumPlaying = infoAlbumOutside;
+
+            pnlClicked.Tag = infoAlbumOutside;
+
+            Navigate.Navigation.Instance.mainScreen.LoadChildAlbum();
+
+            if (infoAlbumOutside.IsArtist == false)
+            {
+                AlbumDAO.Instance.isArtist = false;
+
+                Navigate.Navigation.Instance.childAlbumScreen.LoadTitle(infoAlbumOutside.Image_Genre, infoAlbumOutside.Title);
+
+                Navigate.Navigation.Instance.childAlbumScreen.LoadChildAlbum(infoAlbumOutside.ID_Genre.ToString());
+            }
+            else
+            {
+                AlbumDAO.Instance.isArtist = true;
+
+                Navigate.Navigation.Instance.childAlbumScreen.LoadTitle(infoAlbumOutside.Image_Genre, infoAlbumOutside.Title);
+
+                Navigate.Navigation.Instance.childAlbumScreen.LoadChildAlbum(infoAlbumOutside.Title);
+            }
+
+            Navigate.Navigation.Instance.childAlbumScreen.PlayFirstSong();
+        }
+        private void album_MouseEnterAdd(object sender, EventArgs e)
+        {
+            if (pnlGenreHovered.Tag != null)
+            {
+                Info__Album prevHover = (Info__Album)pnlGenreHovered.Tag;
+
+                prevHover.IsHovered = false;
+            }
+
+            if (sender is Label || sender is Guna2ImageButton || sender is Guna2PictureBox)
+            {
+                Info__Album infoAlbumInside = Info__Album__DAO.Instance.GetInfoAlbumFromControlIntoPanel(sender);
+
+                infoAlbumInside.IsHovered = true;
+
+                pnlGenreHovered.Tag = infoAlbumInside;
+
+                return;
+            }
+
+            Info__Album infoAlbumOutside = Info__Album__DAO.Instance.GetInfoAlbumFromPanel(sender);
+
+            pnlGenreHovered.Tag = infoAlbumOutside;
+
+            infoAlbumOutside.IsHovered = true;
+        }
+        private void album_MouseLeaveAdd(object sender, EventArgs e)
+        {
+            if (sender is Label || sender is Guna2ImageButton || sender is Guna2PictureBox)
+            {
+                Info__Album infoAlbumInside = Info__Album__DAO.Instance.GetInfoAlbumFromControlIntoPanel(sender);
+
+                infoAlbumInside.IsHovered = false;
+
+                return;
+            }
+
+            Info__Album infoAlbumOutside = Info__Album__DAO.Instance.GetInfoAlbumFromPanel(sender);
+
+            infoAlbumOutside.IsHovered = false;
+        }
+
+        #endregion
+
+        #region Navigate
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (pnlClicked.Tag == null)
+
+                return;
+
+            Navigate.Navigation.Instance.mainScreen.LoadChildAlbum();
+        }
+
+        #endregion
+
+        private void LoadEventClick()
+        {
+            Dropdown__Playlist__DAO.Instance.GetAllControls(this);
+        }
+
+        #region Bottom Bar
+        void LoadEventBottomBar()
+        {
+            songPlayingBottomBar.MouseClickAddPlaylist += SongPlayingBottomBar_MouseClickAddPlaylist;
+        }
+
+        private void SongPlayingBottomBar_MouseClickAddPlaylist(object sender, EventArgs e)
+        {
+            Dropdown__Playlist__DAO.Instance.AddPlaylistEventInUserControl(this);
+        }
+
+        public void LoadSongPlayingBottomBar()
+        {
+            songPlayingBottomBar.LoadSongPlayingByInfoSongPanel();
+        }
+
+        public void LoadInitialSongBottomBar()
+        {
+            songPlayingBottomBar.LoadInitialSong();
+
+            LoadEventBottomBar();
+        }
+
+        #endregion
     }
 }
