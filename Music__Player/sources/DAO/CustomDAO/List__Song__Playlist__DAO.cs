c9d1609f2c97db35712077013fb36696957b7c5d
AmosIvor <trantuanvu00000@gmail.com>
2023-02-08 17:05:51 +0700

    add: method to Get List Song By ID Genre or Artist

--- Music__Player/sources/DAO/CustomDAO/List__Song__Playlist__DAO.cs
+++ Music__Player/sources/DAO/CustomDAO/List__Song__Playlist__DAO.cs
@@ -45,6 +45,46 @@ namespace Music__Player.sources.DAO.CustomDAO
             return listSongPlaylist;
         }
 
+        public List<List__Song__Playlist> GetListSongByIdGenre(string idGenre)
+        {
+            List<List__Song__Playlist> listSong = new List<List__Song__Playlist>();
+
+            string query = "Exec PROC_Get_List_Song_By_ID_Genre @id_genre";
+
+            DataTable data = DataProviderDAO.Instance.ExecuteQuery(query, new object[] { Convert.ToInt32(idGenre) }) ;
+
+            foreach (DataRow item in data.Rows)
+            {
+                List__Song__Playlist panel = new List__Song__Playlist(item);
+
+                panel.IsAdd = true;
+
+                listSong.Add(panel);
+            }
+
+            return listSong;
+        }
+
+        public List<List__Song__Playlist> GetListSongByArtist(string nameArtist)
+        {
+            List<List__Song__Playlist> listSong = new List<List__Song__Playlist>();
+
+            string query = "EXEC PROC_Get_List_Song_By_Name_Artist @name_artist";
+
+            DataTable data = DataProviderDAO.Instance.ExecuteQuery(query, new object[] { nameArtist });
+
+            foreach (DataRow item in data.Rows)
+            {
+                List__Song__Playlist panel = new List__Song__Playlist(item);
+
+                panel.IsAdd = true;
+
+                listSong.Add(panel);
+            }
+
+            return listSong;
+        }
+
         public string ConvertID(int id)
         {
             if (id.ToString().Length == 1)
